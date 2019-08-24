6e81145f161145e54d946951ab1dada605257a23
Asus <martz123456@hotmail.com>
2019-08-25 00:04:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toast.makeText(LoginActivity.this, "Welcome to My ToDo application!", Toast.LENGTH_SHORT).show();
+    }
+}
